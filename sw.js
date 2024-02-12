8bf3cf607cad5eb4b9651ce673f5d81eb1c58e3a
brettmrice <brettmrice@gmail.com>
2024-02-12 07:14:34 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,9 +5,7 @@ self.addEventListener('install', event => {
   event.waitUntil((async () => {
     const cache = await caches.open(CACHE_NAME);
     cache.addAll([
-      '/',
-      '/scripts.js',
-      '/styles.css'
+      'index.html'
     ]);
   })());
 });
